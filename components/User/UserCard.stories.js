dfb1c6b8ffa9cd1aa2a6ee6b80a368d044b23f5d
luan0ap <luanap.kun@gmail.com>
2022-08-08 11:19:55 +0100

    App cration, initial components

---
+++ components/User/UserCard.stories.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'User'
+}
+
+export const Card = () => '<user-card firstName="Luan" lastName="Alexandre de Paula" />'
---
+++ components/User/UserCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card
+    outlined
+  >
+    <v-card-title tag="h4">
+      <strong>{{ fullName }}</strong>
+    </v-card-title>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'UserCard',
+
+  props: {
+    firstName: {
+      type: String,
+      required: true
+    },
+
+    lastName: {
+      type: String,
+      default: ''
+    }
+  },
+
+  data () {
+    return {}
+  },
+
+  computed: {
+    fullName () {
+      return `${this.firstName} ${this.lastName}`
+    }
+  }
+}
+</script>
