5a5c82a4d5350833041003f39cc195e64d4695be
luan0ap <luanap.kun@gmail.com>
2022-08-24 16:45:24 +0100

    Create base component for global use

---
+++ components/Base/BasePagination.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-pagination
+    :value="value"
+    circle
+    class="base-pagination"
+    color="inherit"
+    :length="length"
+    @input="$emit('input', $event)"
+    @next="$emit('next')"
+    @previous="$emit('previous')"
+  />
+</template>
+<script>
+export default {
+  name: 'BasePagination',
+
+  props: {
+    value: {
+      type: Number,
+      default: 1
+    },
+
+    length: {
+      validator (value) {
+        return (typeof value === 'string' || typeof value === 'number') && Number(value) > 0
+      },
+      default: 3
+    }
+  },
+
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="scss">
+.base-pagination {
+  > .v-pagination {
+      .v-pagination__item {
+        border-radius: 0;
+        box-shadow: none;
+        border-bottom: 2px solid transparent;
+      }
+
+      .v-pagination__item--active {
+        color: var(--v-primary-base);
+        border-bottom: 2px solid var(--v-primary-base);
+      }
+    }
+}
+</style>
